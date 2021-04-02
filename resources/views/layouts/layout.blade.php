31568c4170b5867e82e3fe0181b92fc6fa6fbe32
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-04-02 14:16:52 +0200

    favicon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,7 +13,7 @@
     <!--<link rel="shortcut icon" href="../../public/favicon.ico" type="image/x-icon">
     <link rel="icon" href="../../public/favicon.ico" type="image/x-icon"> -->
 
-    <link rel="icon" href="{{ URL::asset('/favicon.ico') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ URL::asset('../../public/favicon.ico') }}" type="image/x-icon"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
